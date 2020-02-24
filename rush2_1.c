195c4f243cc220d3caf2503665d512e23856cae2
Gregory Koenig <koenig.gregory@epitech.eu>
2020-02-24 23:03:46 +0100

    master Init project

---
+++ rush2_1.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2019
+** rush2_1
+** File description:
+** C Pool Rush
+*/
+
+int count_letter(char **av)
+{
+    if (av[2] <= 'a' && av[2] >= 'z' && av[2] <= 'A' && av[2] >= 'Z') {
+        for (int i = 0; av[1][i] != '\0'; i++) {
+
+        }
+    }
+}
+
+int main(char **av)
+{
+    count_letter(**av);
+}
